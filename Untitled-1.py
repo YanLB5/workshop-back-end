019bf0cccdbe6861c6709a7459ecaad4046fd7f4
Yan <autologon@CSED.LAB>
2023-03-27 17:12:49 -0300

    Workshop-Day1

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+a =5
+b =8
+soma = a+b
+print(soma)
+
+
+a= input("Digite o número 1:")
+b = input("Digite o número 2:")
+
+if soma == 8:
+        print("A soma é 8")
+else: 
+    print("A soma não é 8")
+    
+print("A soma é: ",soma)
+
+
+
+
